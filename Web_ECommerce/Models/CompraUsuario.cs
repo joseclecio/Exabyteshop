4dfd81a314cfd4bd84c6c3a2f2926d09f074ffc9
Joseclecio <joseclecio.conceicao96@academico.ifs.edu.br>
2021-03-28 00:10:44 -0300

    Commit de atualização

---
+++ Web_ECommerce/Models/CompraUsuario.cs
@@ -0,0 +1,53 @@
+﻿using Entities.Entities.Enums;
+using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Entities
+{
+
+    public class CompraUsuario : Notifies
+    {
+        [Display(Name = "Código")]
+        public int CompraUsuarioID { get; set; }
+
+        [Display(Name = "Produto")]
+        [ForeignKey("Produto")]
+        [Column(Order = 1)]
+        public int ProdutoID { get; set; }
+        public virtual Produto Produto { get; set; }
+
+        [Display(Name = "EnumEstadoCompra")]
+        public EnumEstadoCompra EnumEstadoCompra { get; set; }
+
+        [Display(Name = "Quantidade")]
+        public int Quantidade { get; set; }
+
+
+        [ForeignKey("Usuario")]
+        [Column(Order = 1)]
+        public string UsuarioID { get; set; }
+        public virtual Usuario Usuario { get; set; }
+
+
+        [NotMapped]
+        [Display(Name = "Quantidade Total")]
+        public int QuantidadeProdutos { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Valor Total")]
+        public decimal ValorTotal { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Endereço de entrega")]
+        public string EnderecoCompleto { get; set; }
+
+        [NotMapped]
+        public List<Produto> ListaProdutos { get; set; }
+
+
+    }
+}
---
+++ Web_ECommerce/Models/Produto.cs
@@ -0,0 +1,62 @@
+﻿using Entities.Notifications;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Entities
+{
+
+    public class Produto : Notifies
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [MaxLength(255)]
+        public string Nome { get; set; }
+
+        [Display(Name = "Descrição")]
+        [MaxLength(150)]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Observação")]
+        [MaxLength(20000)]
+        public string Observacao { get; set; }
+
+        [Display(Name = "Valor")]
+        public decimal Valor { get; set; }
+
+        [Display(Name = "Quantidade Estoque")]
+        public int QuantidadeEstoque { get; set; }
+
+        [Display(Name = "Usuário")]
+        [ForeignKey("Usuario")]
+        [Column(Order = 1)]
+        public string UsuarioID { get; set; }
+        public virtual Usuario Usuario { get; set; }
+
+
+        [Display(Name = "EnumEstadoCompra")]
+        public bool Estado { get; set; }
+
+        [Display(Name = "Data de Cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        [Display(Name = "Data de Alteração")]
+        public DateTime DataAlteracao { get; set; }
+
+        [NotMapped]
+        public int IdProdutoCarrinho { get; set; }
+
+        [NotMapped]
+        public int QtdCompra { get; set; }
+
+        [NotMapped]
+        public IFormFile Imagem { get; set; }
+
+        public string Url { get; set; }
+    }
+}
